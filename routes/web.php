78b625d66b63deffc51bb8f3b8470f4187cee5e6
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2024-02-03 16:59:14 +0530

    Sending Mail through sync Method Takes time

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendTestMail;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Mail::to('abc@gmail.com')->send(new SendTestMail());
+
     return view('welcome');
 });
